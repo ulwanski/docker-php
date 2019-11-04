eed2eebdc958e9e20c6d6bcebd6764b9e3382fbe
Marek Ulwański <marek@ulwanski.pl>
2019-11-04 22:50:32 +0100

    Update php7.3

--- 7.3/build-all.sh
+++ 7.3/build-all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker build --compress -t ulwanski/php:7.2.6-fpm -f php7.2.6-fpm .
-docker build --compress -t ulwanski/php:7.2.6-fpm-dev -f php7.2.6-fpm-dev .
+docker build --compress -t ulwanski/php:7.3-cli -f php7.2.6-cli .
+docker build --compress -t ulwanski/php:7.3-fpm -f php7.2.6-fpm .
+docker build --compress -t ulwanski/php:7.3-fpm-dev -f php7.2.6-fpm-dev .
